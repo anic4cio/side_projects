071b4612598c635674d2781ffb07939ac14f988b
anic4cio <anicaciovinicius36@gmail.com>
2022-05-24 11:34:44 -0300

    Objeto window

---
+++ POO_javascript/14_obj_window/index.js
@@ -0,0 +1,21 @@
+var teste = 'teste';
+var testamento = 'testamento';
+
+let testeLocal = 'testeLocal';
+
+const testeConst = 'Contante teste';
+
+
+function testando(teste) {
+    teste = 'teste';
+    console.log(teste);
+}
+
+
+console.log(teste);
+console.log(this.teste);
+console.log(window.teste);
+console.log(this.window);
+
+
+console.log(window);
