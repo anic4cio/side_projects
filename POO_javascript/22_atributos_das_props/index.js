4567ca308e5542a88f79f9c58991c04455b7ec9f
anic4cio <anicaciovinicius36@gmail.com>
2022-05-24 11:38:45 -0300

    Atributos das propriedades

---
+++ POO_javascript/22_atributos_das_props/index.js
@@ -0,0 +1,10 @@
+let person = {
+    name: "Adriane"
+}
+
+// person -> Object
+
+console.log(Object.getOwnPropertyDescriptor(person, 'name'));
+// console.log(Object.getOwnPropertyDescriptor(window, 'scrollX'));
+
+// Verificar abributos default dos objetos
