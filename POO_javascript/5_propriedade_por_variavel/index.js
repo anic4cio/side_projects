ae7ee4caf1dfafd51eb22085ce2eb47ab3b0a4c7
anic4cio <anicaciovinicius36@gmail.com>
2022-05-24 11:27:21 -0300

    Acessando propriedades por variável

---
+++ POO_javascript/5_propriedade_por_variavel/index.js
@@ -0,0 +1,16 @@
+const motorbike = {
+    motor: 1000,
+    corporation: 'Yamaha',
+    bikeName: 'R1',
+    fabrication: 2021,
+    color: 'Blue',
+    available: true
+}
+
+// Acessando propriedades com variáveis
+
+let bike = 'bikeName';
+
+console.log(motorbike[bike]);
+
+console.log(motorbike.bikeName)
