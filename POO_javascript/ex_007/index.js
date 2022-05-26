3701bac3ef61648a0735d6c74aae7a022fff2f37
anic4cio <anicaciovinicius36@gmail.com>
2022-05-26 09:34:57 -0300

    Verificando se um objeto é uma instância de outro

---
+++ POO_javascript/ex_007/index.js
@@ -0,0 +1,12 @@
+let sport = "black bird";
+let custom = 'night shadow';
+let trail = 'cabavo de troia';
+let naked = 'ninja';
+let street = 'titan';
+
+let firstGarage = {sport, custom, trail, naked, street};
+
+let secondGarage = {sport, custom, trail, naked, street};
+
+console.log(firstGarage);
+console.log(secondGarage);
