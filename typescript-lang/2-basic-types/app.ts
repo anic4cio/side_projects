3dae0fd9712535358fc16bd59d8652e437e84018
anic4cio <anicaciovinicius36@gmail.com>
2022-06-06 15:38:39 -0300

    Variables Typescript

--- typescript-lang/2-basic-types/app.ts
+++ typescript-lang/2-basic-types/app.ts
@@ -1,8 +1,8 @@
-let car: string = "Lamborghini";
+let car: string = "Lamborghini Gallardo";
 let wheels: number = 4;
 let sunRoof: boolean = true;
 
-console.log(car); // Lamborghini
+console.log(car); // Lamborghini Gallardo
 console.log(wheels); // 4
 console.log(sunRoof); // true
 
