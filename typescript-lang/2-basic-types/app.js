a23666b2b6434badba1a62b911ad63edf4f57ec5
anic4cio <anicaciovinicius36@gmail.com>
2022-06-06 15:26:02 -0300

    Variables Typescript

---
+++ typescript-lang/2-basic-types/app.js
@@ -0,0 +1,6 @@
+var car = "Lamborghini";
+var wheels = 4;
+var sunRoof = true;
+console.log(car); // Lamborghini
+console.log(wheels); // 4
+console.log(sunRoof); // true
---
+++ typescript-lang/2-basic-types/app.ts
@@ -0,0 +1,8 @@
+let car: string = "Lamborghini";
+let wheels: number = 4;
+let sunRoof: boolean = true;
+
+console.log(car); // Lamborghini
+console.log(wheels); // 4
+console.log(sunRoof); // true
+
