1295b2b185e1e5e985d550784d0025db65f97429
anic4cio <anicaciovinicius36@gmail.com>
2022-06-06 19:55:24 -0300

    Typescript Functions

---
+++ typescript-lang/12-functions/app.ts
@@ -0,0 +1,6 @@
+function salutation(salute: string, name: string): string {
+    return `Hello ${salute} ${name}, how are you?`
+}
+
+console.log(salutation('Mrs.', 'Noêmia')); // Hello Mrs. Noêmia, how are you?
+console.log(salutation('Mano', 'Brown')); // Hello Mano Brown, how are you?
