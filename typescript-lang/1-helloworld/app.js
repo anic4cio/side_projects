7308f040b7c8ba62c1bed7198c98214f920d49af
anic4cio <anicaciovinicius36@gmail.com>
2022-06-06 15:18:08 -0300

    Hello world Typescript

---
+++ typescript-lang/1-helloworld/app.js
@@ -0,0 +1,6 @@
+function helloWorld(person, lastname) {
+    return "Say hello world, ".concat(person, " ").concat(lastname, "!");
+}
+var person = 'Vinicius';
+var lastname = 'Anicácio';
+console.log(helloWorld(person, lastname));
---
+++ typescript-lang/1-helloworld/app.ts
@@ -0,0 +1,7 @@
+function helloWorld(person: string, lastname: string) {
+    return `Say hello world, ${person} ${lastname}!`
+}
+
+let person = 'Vinicius';
+let lastname = 'Anicácio';
+console.log(helloWorld(person, lastname));
