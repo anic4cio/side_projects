0d0d11886144a8f29242de7f2520b9b1b420f2b4
anic4cio <anicaciovinicius36@gmail.com>
2022-06-06 15:39:43 -0300

    Arrays and objects in Typescript

--- typescript-lang/3-array-obj/app.ts
+++ typescript-lang/3-array-obj/app.ts
@@ -1,10 +1,10 @@
 // first way to do an array on typescript
-const fruits: string[] = ['Banana', 'Apple', 'Orange'];
+const fruits: string[] = ['Banana', 'Strawberry', 'Pinapple'];
 
 // another way
 let numbers: Array<number> = [1,2,3,4,5];
 
-console.log(fruits); // ['Banana', 'Apple', 'Orange']
+console.log(fruits); // ['Banana', 'Strawberry', 'Pineapple']
 console.log(numbers) // [1,2,3,4,5]
 
 let obj = {
