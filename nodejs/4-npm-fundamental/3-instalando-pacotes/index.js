82884146355d53e09398ff5bd8a84bdc471816da
anic4cio <anicaciovinicius36@gmail.com>
2022-06-11 15:34:49 -0300

    Instalando o modulo lodash

--- nodejs/4-npm-fundamental/3-instalando-pacotes/index.js
+++ nodejs/4-npm-fundamental/3-instalando-pacotes/index.js
@@ -3,7 +3,7 @@ const _ = require('lodash')
 const a = [1,4,6,7,9,8]
 const b = [2,3,5,8,7,4]
 
-// diferência dos dois arrays
+// diferênça entre os dois arrays
 const diff = _.difference(a, b) // [ 1, 6, 9 ]
 
 console.log(diff)
