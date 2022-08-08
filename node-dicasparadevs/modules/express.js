facb59a8fb44edbb6d2d022f0f2936194a6fbf71
anic4cio <anicaciovinicius36@gmail.com>
2022-08-07 21:35:47 -0300

    CRUD, MongoDB and MiddleWares

--- node-dicasparadevs/modules/express.js
+++ node-dicasparadevs/modules/express.js
@@ -3,8 +3,32 @@
 // importa o express depois de instalado
 const express = require('express')
 
+//importa o meu modulo users.model
+const UserModel = require('../src/models/user.model')
+
 // instancia o express numa constante "app"
 const app = express()
+
+// faz o express reconhecer um json na requisição
+app.use(express.json());
+
+// EJS (View Engine)
+app.set('view engine', 'ejs');
+app.set('views', 'src/views')
+
+app.get('/views/users', async (req, res) => {
+    const users = await UserModel.find({})
+    res.render('index', { users }); 
+});
+
+// MIDDLEWARES (exibe informações do servidor ao estabelecer uma conexão)
+app.use((req, res, next) => {
+    console.log(`Request Type: ${req.method}`);
+    console.log(`Content Type: ${req.headers["content-type"]}`);
+    console.log(`Date: ${new Date()}`);
+
+    next();
+});
 
 // inicia o server com um h1 de html, no caminho home
 app.get('/home', (req, res) => {
@@ -12,20 +36,61 @@ app.get('/home', (req, res) => {
 })
 
 // adiciona um pagina ao servidor, users, com um objeto contendo informações
-app.get('/users', (req, res) => {
-    const users = [
-        {
-            name: 'Jordan Stirling',
-            email: 'jordan@email.com'
-        },
-        {
-            name: 'Sônia Shumacher',
-            email: 'sonia@email.com'
-        }
-    ]
+app.get('/users', async (req, res) => {
+    try {
+        const users = await UserModel.find({});
+        res.status(200).json(users)
 
-    res.status(200).json(users);
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+});
+
+// endpoint de listagem de um usuário por ID com GET
+app.get('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findById(id)
+        return res.status(200).json(user)
+
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+});
+
+// criar usuário com POST
+app.post('/users', async (req, res) => {
+    try {
+        const user = await UserModel.create(req.body);
+        res.status(201).json(user);
+
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
 })
+
+// atualizar informações de usuário com PATCH
+app.patch('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+// deletar um usuário com DELETE
+app.delete('/users/:id', async(req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndDelete(id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+});
+
 
 const port = 8080;
 
