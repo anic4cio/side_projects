bf2e838ed29eca26b7bb48b11cfaac602bff8156
anic4cio <anicaciovinicius36@gmail.com>
2022-08-24 12:20:28 -0300

    Simple fetch API with Axios

---
+++ Javascript/async-await/axios.js
@@ -0,0 +1,68 @@
+const axios = require('axios');
+
+const loginUser = (email, password) => {
+    return new Promise((resolve, reject) => {
+        const error = false;
+
+        if (error) {
+            reject(new Error('Login error!'))
+        }
+
+        console.log('Login successful!');
+        resolve({ email, password })
+    });
+}
+
+const getUserElements = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(getUserElements);
+            resolve(['element1', 'element2', 'element3'])
+        }, 2000);
+    });
+}
+
+const getElementsDetails = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(getElementsDetails);
+            resolve({detail: '**Element details**'})
+        }, 2500);
+    });
+}
+
+const displayUser = async() => {
+    try {
+        const user = await loginUser('vic@email.com', '123');
+        const elements = await getUserElements()
+        const elementDetails = await getElementsDetails(elements[0])
+    
+        console.log([ user ]); // [ { email: 'vic@email.com', password: '123' } ]
+    
+        console.log([ elements ]); // [ [ 'element1', 'element2', 'element3' ] ]
+    
+        console.log([ elementDetails ]); // [ { detail: '**Element details**' } ]
+
+    } catch(error) {
+        console.log({ error });
+    }
+}
+
+// axios.get('https://jsonplaceholder.typicode.com/users/todos/1')
+//     .then((response) => {
+//         console.log({response});
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     })
+
+const fetchApi = async() => {
+    try {
+        const { data } = await axios.get('https://jsonplaceholder.typicode.com/users/todos/1')
+        console.log({data})
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+fetchApi();
