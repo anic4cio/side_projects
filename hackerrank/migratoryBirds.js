b1307827a4234234940217d3812879bdc2d4601a
anic4cio <anicaciovinicius36@gmail.com>
2022-08-25 11:53:45 -0300

    almost reaching, only two testes remaining

--- hackerrank/migratoryBirds.js
+++ hackerrank/migratoryBirds.js
@@ -1,17 +1,24 @@
-function migratoryBirds(arr = []) {
-    let birdsQty = 0;
-    let sortedArr = arr.sort();
-    for(let element = 0; element >= arr.length; element++) {
+// "use strict"
 
-        if (sortedArr.filter(item => item == element).length > birdsQty) {
-            birdsQty = element;
+function migratoryBirds(arr) {
+    arr.sort().reverse();
+    let target = 0;
+    let occurrence = Math.max(...arr);
+    let leng = 0
+    let currentLeng = 0
+
+    for(let i = 0; i <= arr.length; i++){
+        target = arr.filter(x => x == arr[i]);
+        leng = arr.filter(x => x == arr[i]).length;
+        if (target[0] < occurrence && leng >= currentLeng) {
+            currentLeng = leng
+            occurrence = target[0] 
         }
-    };
-    return birdsQty
+    }
+    return occurrence
 }
 
 // Está falhando em metade dos testes
 
 const birds = [6, 6, 6, 1, 4, 4, 4, 5, 3];
-
 console.log(migratoryBirds(birds));
