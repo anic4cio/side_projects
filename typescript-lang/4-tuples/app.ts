29c4b9c0bbf7ab6cfe88ef794784fc5f787c0f61
anic4cio <anicaciovinicius36@gmail.com>
2022-09-23 17:24:11 -0300

    Readonly custom type

--- typescript-lang/4-tuples/app.ts
+++ typescript-lang/4-tuples/app.ts
@@ -1,5 +1,11 @@
 let plane: [string, boolean, number, string];
-
 plane = ['Boeing', true, 7, 'Flying'];
-
 console.log(plane);
+
+var obj: { readonly name: string } = { name : ''};
+function changing(obj: { name: string }) {
+    obj.name = 'Someone\'s Name';
+};
+
+changing(obj);
+console.log(obj);
