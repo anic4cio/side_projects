be451c32923bfaada1657be29c7819d0e3ab68fc
anic4cio <anicaciovinicius36@gmail.com>
2022-09-23 17:22:21 -0300

    Custom types

--- typescript-lang/2-basic-types/app.ts
+++ typescript-lang/2-basic-types/app.ts
@@ -6,3 +6,13 @@ console.log(car); // Lamborghini Gallardo
 console.log(wheels); // 4
 console.log(sunRoof); // true
 
+
+// Custom types
+
+type tipo1 = string | number | boolean
+type tipo2 = string | number 
+
+let t: tipo1 | tipo2
+t = 'oi'
+t = false
+t = 7
