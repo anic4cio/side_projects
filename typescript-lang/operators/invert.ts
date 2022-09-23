dd1d8e8f5e8127029cf8a3df295a5d608bcf3745
anic4cio <anicaciovinicius36@gmail.com>
2022-09-23 17:24:20 -0300

    Operators

---
+++ typescript-lang/operators/invert.ts
@@ -0,0 +1,19 @@
+let a = 10
+let b = 50
+let c = 'Abraão'
+let d = false
+let e = true
+
+// console.log(~a) // -11
+// console.log(~b) // -51
+// console.log(~c) // -1
+// console.log(~d) // -1
+// console.log(~e) // -2
+
+//Operadores Bit a Bit
+console.log(a << b) // 2621440
+console.log(a >> b) // 0
+console.log(a ^ b)  // 56
+console.log(a & b)  // 2
+console.log(a >>> b)// 0
+console.log(a > b)  // false
---
+++ typescript-lang/operators/unary.ts
@@ -0,0 +1,17 @@
+let dez = 10
+console.log(dez++)  // 10
+console.log(dez)    // 11
+console.log(dez++)  // 11
+console.log(dez)    // 12
+
+/* 
+Importante se atentar que a ordem do operador altera no print
+do console.log, colocando o operador ++/-- com prefixo ou sufixo
+*/
+
+let vinte = 20
+console.log(--vinte)  // 19
+console.log(vinte)    // 19
+console.log(--vinte)  // 18
+console.log(vinte)    // 18
+
