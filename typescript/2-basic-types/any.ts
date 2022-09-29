6e3de7ac52b3e4032c135096c1b5cc5c2c80d139
anic4cio <anicaciovinicius36@gmail.com>
2022-09-29 15:38:51 -0300

    any is any type, that's it

---
+++ typescript/2-basic-types/any.ts
@@ -0,0 +1,11 @@
+let unknow: any;
+
+unknow = 'whatever';
+unknow = 4;
+unknow = false;
+
+console.log(unknow); // false
+
+let unknowList: any[] = [8, 'Yellow', true, 'Santa Monica'];
+
+console.log(unknowList); // [8, 'Yellow', true, 'Santa Monica']
