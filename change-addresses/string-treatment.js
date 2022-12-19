f3a8fa59d946b082bd7cc090c7da13d9dd8eb5b1
anic4cio <anicaciovinicius36@gmail.com>
2022-12-19 17:22:26 -0300

    [feat] - new fields to JSON

--- change-addresses/string-treatment.js
+++ change-addresses/string-treatment.js
@@ -10,20 +10,29 @@ const address = /(no\(a\)\s*)(.*?)(\d{2}\/\d{2}\/\d{4}, )/g
 
 const replaceFields = (arrayOfBanns) => {
   const newBanns = arrayOfBanns.map(bann => {
-    const newTeor = bann.meta.teor.replaceAll(/[\r\n]/gm, '').replaceAll(/  +/gm, ' ')
-    const fieldNacionalityAndWork = newTeor.match(regexNacionalityAndWork)[0]
-    const fieldNacionalityAndWork2 = newTeor.match(regexNacionalityAndWork)[1]
+    const teor = bann.meta.teor.replaceAll(/[\r\n]/gm, '').replaceAll(/  +/gm, ' ')
+    const fieldNacionalityAndWork = teor.match(regexNacionalityAndWork)[0]
+    const fieldNacionalityAndWork2 = teor.match(regexNacionalityAndWork)[1]
     const withoutNationality = fieldNacionalityAndWork.replaceAll(regexNacionality, '')
     const withoutNationality2 = fieldNacionalityAndWork2.replaceAll(regexNacionality, '')
     const removedWork = withoutNationality.replaceAll(regexWork, '')
     const removedWork2 = withoutNationality2.replaceAll(regexWork, '')
-    const finalField = newTeor.replaceAll(fieldNacionalityAndWork, removedWork)
+    const finalField = teor.replaceAll(fieldNacionalityAndWork, removedWork)
     const finalField2 = finalField.replaceAll(fieldNacionalityAndWork2, removedWork2)
-    console.log(finalField2.replaceAll(address, ''), '\n')
-    // return 
+    const newTeor = finalField2.replaceAll(address, '')
+    return newTeor
   })
   return newBanns
 }
 
-const oneTime = replaceFields(arrayOfBanns)
-// console.log(oneTime)
+const newArrayOfBanns = arrayOfBanns => {
+  const newTeorField = replaceFields(arrayOfBanns)
+  for (let i = 0; i < arrayOfBanns.length; i++) {
+    arrayOfBanns[i].meta.teor = newTeorField[i]
+  }
+  return arrayOfBanns
+}
+
+const a = newArrayOfBanns(arrayOfBanns)
+const jsonBuffer = Buffer.from(JSON.stringify(a))
+fs.writeFileSync('newPosts.json', jsonBuffer)
