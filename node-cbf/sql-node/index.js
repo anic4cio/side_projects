fadf02e3e61fdb9d0e0010419eca75da29146d51
anic4cio <anicaciovinicius36@gmail.com>
2023-01-11 16:29:51 -0300

    feat: calling CRUD functions

--- node-cbf/sql-node/index.js
+++ node-cbf/sql-node/index.js
@@ -1,10 +1,29 @@
-(async () => {
+(async function showClients() {
   const database = require('./database.js')
-  console.log('Insert new client')
-  const name = 'Amayomi'
-  const value = 28
-  await database.addClient({ name, value })
-  console.log('Get all clients')
+  console.log('Getting all clients...')
   const clients = await database.getAllClients()
-  console.log(clients)
-})()
+  return clients.forEach(client => console.log(client))
+});
+
+(async function addClient() {
+  const database = require('./database.js')
+  const id = 11
+  const name = 'Nubia'
+  const table = { id, name }
+  await database.addClient(table)
+  console.log(`Client ${JSON.stringify(table)} added`)
+});
+
+(async function updateClient() {
+  const database = require('./database.js')
+  const table = { id: 10, name: 'Angelica' }
+  await database.updateClient(table)
+  console.log(`Table ${JSON.stringify(table)} updated`)
+});
+
+(async function removeClient() {
+  const database = require('./database.js')
+  const id = 1
+  await database.removeClient(id)
+  console.log(`Client ${id} removed`)
+});
