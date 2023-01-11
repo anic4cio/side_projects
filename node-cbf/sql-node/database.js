95b89314eaee7b72f67fb43c1063dcc64cf89909
anic4cio <anicaciovinicius36@gmail.com>
2023-01-11 16:29:18 -0300

    refactor: returns' message

--- node-cbf/sql-node/database.js
+++ node-cbf/sql-node/database.js
@@ -1,38 +1,45 @@
 const connect = async () => {
-  if (global.con && global.con.state !== 'disconnected')
-    return global.con
+  if (global.wire && global.wire.state !== 'disconnected')
+    return global.wire
   const mysql = require('mysql2/promise');
-  const connection = mysql.createConnection('mysql://root:root@localhost:3306')
+  const connection = mysql.createConnection('mysql://root:2345@127.0.0.1:3306/twister')
   console.log('Connected!')
-  global.con = connection
+  global.wire = connection
   return connection
 }
 
 const getAllClients = async () => {
-  const con = await connect()
-  const [rows] = await con.execute('SELECT * FROM clients_node')
-  return await rows
+  const connection = await connect()
+  const [rows] = await connection.execute('SELECT * FROM clients')
+  connection.close()
+  return rows
 }
 
 const addClient = async (client) => {
-  const con = await connect()
-  const sql = 'INSERT INTO client_node (name, value) VALUES (?,?)'
-  const values = [client.name, client.value]
-  await con.query(sql, values)
+  const { id, name } = client
+  const connection = await connect()
+  const sql = 'INSERT INTO clients (id, name) VALUES (?,?)'
+  const values = [id, name]
+  await connection.query(sql, values)
+  connection.close()
 }
 
-const updateClient = async (id, client) => {
-  const con = await connect()
-  const sql = 'UPDATE client_node SET name=?, value=? WHERE id=?'
-  const values = [client.name, client.value, id]
-  await con.query(sql, values)
+const updateClient = async (client) => {
+  const { id, name } = client
+  const connection = await connect()
+  const sql = 'UPDATE clients SET id=?, name=? WHERE id=?'
+  const values = [id, name, id]
+  await connection.query(sql, values)
+  connection.close()
 }
 
 const removeClient = async (id) => {
-  const con = await connect()
-  const sql = 'DELETE FROM client_node WHERE id=?'
+  const connection = await connect()
+  const sql = 'DELETE FROM clients WHERE id=?'
   const values = [id]
-  await con.query(sql, values)
+  connection.close()
+  await connection.query(sql, values)
+  connection.close()
 }
 
 module.exports = {
