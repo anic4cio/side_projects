f9bbe2784e0350dd053d8022d41d7ead6fc0df3d
anic4cio <anicaciovinicius36@gmail.com>
2023-01-18 13:59:16 -0300

    occasionally I found the solution

--- typescript/arrayMethods/problem.ts
+++ typescript/arrayMethods/problem.ts
@@ -22,3 +22,32 @@ console.log(isCousinPrimeExist === cousins) // false
 // I am not sure if this is a bug or I am doing something wrong.
 
 //  WHY THIS IS FALSE?
+
+// I FOUND THE SOLUTION:
+// 1. I have to use JSON.stringify() to compare arrays
+
+console.log(JSON.stringify(isCousinPrimeExist) === JSON.stringify(cousins)) // true
+
+// EXPLANATION:
+
+// The reason why this is false is because arrays are objects, and objects are compared by reference, not by value.
+// So, when you compare two arrays, you are actually comparing if they are the same object.
+// Two different arrays can have the same values, but they are not equal, because they are not the same object.
+// If you want to compare the values of two arrays, you have to convert them to strings and compare the strings.
+// The JSON.stringify() method converts a JavaScript object or value to a JSON string.
+// The JSON.parse() method parses a JSON string, constructing the JavaScript value or object described by the string.
+
+/*
+Comparisons between types
+Internally, it .includes() makes a comparison through ===. However,
+comparisons with === or == only work as expected (compare value) with the types:
+
+Boolean
+Null
+Undefined
+Number
+String
+Symbol (ES2015)
+
+When we compare variables that reference the type object, the interpreter will test whether they point to the same object in memory.
+*/
